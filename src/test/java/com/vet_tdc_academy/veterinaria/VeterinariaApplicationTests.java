3c1b4e3c04c2538ee674ad354eaa8171cc6becd4
gonza desktop <gomezgonzalo2012@gmail.com>
2024-02-24 10:08:53 -0300

    ejercicio veterinaria tdc academy

---
+++ src/test/java/com/vet_tdc_academy/veterinaria/VeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vet_tdc_academy.veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
